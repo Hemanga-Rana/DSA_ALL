a481147559b186172ccf856a17c893d67c7266fb
HemangaRana <hemanga.rana1@gmail.com>
2022-09-12 23:50:07 +0530

    Second Approch

--- Array/2_min_and_max.cpp
+++ Array/2_min_and_max.cpp
@@ -47,5 +47,15 @@ int main()
     cout<<"Maximum value "<<max<<endl;
     cout<<"Minimum value "<<min;
 
+
+
+    // Second Approch O(nlogn)
+    // int arr[]= {2,4,1,9,5};
+    // int size = sizeof(arr)/sizeof(int);
+    // sort(arr, arr+size);
+    // cout<<"min: "<<arr[0]<<endl;
+    // cout<<"max: "<<arr[size-1]<<endl;
+
+
     return 0;
 }
