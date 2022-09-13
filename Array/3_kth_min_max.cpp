a2cb9f328f9657094f2b275e2fea60141e2e0b6a
HemangaRana <hemanga.rana1@gmail.com>
2022-09-13 08:09:25 +0530

    new approch added

--- Array/3_kth_min_max.cpp
+++ Array/3_kth_min_max.cpp
@@ -1,4 +1,4 @@
-// Find the "Kth" max and min element of an array.
+// Find the "Kth" max and min element of an array. O(nlogn)
 // N=6
 // arr[] = 7 10 4 3 20 15
 // K=3;
@@ -22,4 +22,50 @@ int main()
     int r=size-1;
     int k =3;
     cout<<kthSmallest(arr,l,r,k)<<endl;
+}
+
+// ************************************
+// Best Approch O(n)
+
+void printArray(int arr[], int n )
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void sort012(int a[], int size)
+{
+    int low =0;
+    int high = size-1;
+    int mid =0;
+
+    while(mid<=high)
+    {
+        switch(a[mid])
+        {
+            case 0:
+                swap(a[low++], a[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(a[mid], a[high--]);
+                break;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {0,1,1,0,1,2,1,2,0,0,0,1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    sort012(arr,n);
+    printArray(arr,n);
+
+    return 0;
 }
