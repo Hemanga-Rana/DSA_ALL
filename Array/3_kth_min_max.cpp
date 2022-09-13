52a9510bfcd897ecfb43991a7342d94893fc802b
HemangaRana <hemanga.rana1@gmail.com>
2022-09-13 07:46:02 +0530

    kth min max new added

---
+++ Array/3_kth_min_max.cpp
@@ -0,0 +1,25 @@
+// Find the "Kth" max and min element of an array.
+// N=6
+// arr[] = 7 10 4 3 20 15
+// K=3;
+// Output: 7
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    sort(arr, arr+r+1);
+    return arr[k-1];
+}
+
+int main()
+{
+    int arr[]={2,7,5,3,6};
+    int size = sizeof(arr)/sizeof(int);
+    int l=0;
+    int r=size-1;
+    int k =3;
+    cout<<kthSmallest(arr,l,r,k)<<endl;
+}
